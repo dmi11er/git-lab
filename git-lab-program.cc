82c6f5cc8c3a5e66cc2647f7ea17d3294e0c342f
Dylan <dm097021@ohio.edu>
2022-01-18 19:06:42 -0500

    Fourth Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+* File: git-lab-program.cc
+* Author: Dylan Miller
+* Date : 1/18/22
+* Description: Lab 2
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc,char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
